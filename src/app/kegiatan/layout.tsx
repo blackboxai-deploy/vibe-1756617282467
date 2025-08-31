04e0a0453fb55c4bf6e46451338a6745211b2b97
blackboxai-deploy <deploy@blackboxai.dev>
2025-08-31 10:44:56 +0530

    Add src/app/kegiatan/layout.tsx

---
+++ src/app/kegiatan/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Sidebar } from '@/components/layout/Sidebar';
+import { Header } from '@/components/layout/Header';
+
+export default function KegiatanLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="flex">
+        {/* Desktop Sidebar */}
+        <div className="hidden md:flex">
+          <Sidebar />
+        </div>
+        
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1 p-4 md:p-6">
+            {children}
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
